cc4ca724d9f1adfee2d4a469d2093b45beb778a8
BashaVali <bashavali222@gmail.com>
2022-11-02 02:59:44 +0530

    Basic Problems

---
+++ BasicPrograms/HarmonicSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class HarmonicSeries
+    {
+        public int InputValue()
+        {
+            Console.WriteLine("Enter the Number to find the Harmonic value : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            return N;
+        }
+        public void Harmonic()
+        {   
+             // loop to apply the formula 
+
+    // Hn = H1 + H2 + H3 ... + 
+
+    // Hn-1 + Hn-1 + 1/n
+
+            int N = InputValue();
+            float harmonic = 1;
+            for (int i = 2; i < N; i++)
+            {
+                harmonic += (float)1/ N;
+            }
+            Console.WriteLine("The Nth Harmonic Value" + N + " : " + harmonic);
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,8 +1,5 @@
-﻿using BasicPrograms;
-using BasicPrograms.LeapYear;
+﻿using BasicPrograms.LeapYear;
 using BasicPrograms.PowerOf2;
-using System;
-
 namespace BasicPrograms
 {
     class Program
@@ -15,6 +12,9 @@ namespace BasicPrograms
             leapYear.LeapYear();
             PowerOf2s powerOf2 = new PowerOf2s();
             powerOf2.PowerOf2();
+            HarmonicSeries harmonic = new HarmonicSeries();
+            harmonic.Harmonic();
+            
         }
     }
 }
