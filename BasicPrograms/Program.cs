58e5222cb3e4693c2a58c7e5225870cfb7a50510
BashaVali <bashavali222@gmail.com>
2022-11-01 16:14:52 +0530

    Flip Coin

---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using BasicPrograms;
+using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoins flipCoin = new FlipCoins();
+            flipCoin.FlipCoin();
+        }
+    }
+}
